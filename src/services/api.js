b75f2755ac93c3e05139c6d7b2e61b07b47b2fe4
Josh L <joshua.levine42@gmail.com>
2025-01-14 14:50:56 -0500

    Create function working

--- src/services/api.js
+++ src/services/api.js
@@ -6,30 +6,46 @@ const api = axios.create({
   headers: {
     Authorization: `Bearer ${localStorage.getItem("authToken")}`,
   },
-//   withXSRFToken: true,
-//     xsrfCookieName: `csrftoken`,
-//     xsrfHeaderName: `X-CSRFToken`,
+  //   withXSRFToken: true,
+  //     xsrfCookieName: `csrftoken`,
+  //     xsrfHeaderName: `X-CSRFToken`,
 });
 
-
-
-
-
-export const registerUser = (data) => api.post('/auth/register/', data);
-export const loginUser = (data) => api.post('/auth/login/', data)
-    .then(response => {
-        localStorage.setItem('authToken', response.data.token);
-        return response
-    })
-export const fetchProfile = (userId) => api.get(`users/profiles/${userId}`)
+export const registerUser = (data) => api.post("/auth/register/", data);
+export const loginUser = (data) =>
+  api.post("/auth/login/", data).then((response) => {
+    localStorage.setItem("authToken", response.data.token);
+    return response;
+  });
+export const fetchProfile = (userId) => api.get(`users/profiles/${userId}`);
 // export const loginUser = async (credentials) => {
 //   const res = await axios.post("/auth/login/", credentials);
 //   return res
 // }
-export const fetchPosts = () => api.get('/blogposts');
+export const fetchPosts = () => api.get("/blogposts");
 export const fetchPostById = (id) => api.get(`/blogposts/${id}`);
 
 export const fetchUserById = (userId) => api.get(`/users/${userId}`);
-export const handleCreatePost = (data) => api.post('/blogposts/', data);
+export const handleCreatePost = async (data) => {
+  try {
+    const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}blogposts/`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+      },
+    });
+    if(res.ok){
+      console.log('Post created successfully');
+      const results = await res.json();
+      console.log(results);
+    }
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.error("Error creating post:", error);
+  }
+};
 
 export default api;
